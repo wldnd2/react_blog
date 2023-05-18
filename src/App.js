2fb8c036ab2aae5f71d2e4cb2cf838c7d4b7b9c8
wldnd2 <jiwoong000628@gmail.com>
2023-05-18 09:57:45 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState(["한식 맛집", "일식 맛집", "양식 맛집"]);
+  const [like, setLike] = useState([0, 0, 0]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+  const [t, setT] = useState("");
+
+  const openModal = (index) => {
+    setShowModal(!showModal);
+    setSelectedItemIndex(index);
+  };
+
+  const handleLikeClick = (index) => {
+    setLike((prevLike) => {
+      const temp = [...prevLike];
+      temp[index] += 1;
+      return temp;
+    });
+  };
+
+  const handleDeleteClick = (index) => {
+    setTitle((prevTitle) => {
+      const temp = [...prevTitle];
+      temp.splice(index, 1);
+      return temp;
+    });
+  };
+
+  const renderList = () => {
+    return title.map((item, index) => (
+      <div className="list" key={index}>
+        <h4 onClick={() => openModal(index)}>
+          {item} <span onClick={() => handleLikeClick(index)}>🔥</span>
+          {like[index]}
+        </h4>
+        <span>5/14 발행</span>
+        <button onClick={() => handleDeleteClick(index)}>클릭하면 삭제됨</button>
+      </div>
+    ));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>blog</div>
+      </div>
+      {renderList()}
+      {showModal && selectedItemIndex !== null && (
+        <Modal title={title[selectedItemIndex]} />
+      )}
+      <input type="text" onChange={(e) => setT(e.target.value)} />
+      <button onClick={() => {
+        const copy = [...title];
+        copy.unshift(t);
+        setTitle(copy);
+        const temp = [...like];
+        temp.unshift(0);
+        setLike(temp);
+      }}>submit</button>
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h4>{props.title}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
